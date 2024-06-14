eb272d7aa36f733a42e20644e2cba7a939d0c43e
Mr. Hong <rammus1018@naver.com>
2024-06-14 15:18:59 +0900

    fix : 플러그인 경로 재변경

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -7,6 +7,10 @@
     h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
 })(document);
 
-import $ from '../../static/plugin/jQuery/jquery-3.7.1.min.js';
+var jquery = document.createElement('script');
+jquery.src = '../plugin/jQuery/jquery-3.7.1.min.js';
+document.head.appendChild(jquery);
 
-import '../../static/plugin/lcdsantos-jQuery-Selectric/src/jquery.selectric.js';
+var selectricScript = document.createElement('script');
+selectricScript.src = '../../static/plugin/lcdsantos-jQuery-Selectric/src/jquery.selectric.js';
+document.head.appendChild(selectricScript);
