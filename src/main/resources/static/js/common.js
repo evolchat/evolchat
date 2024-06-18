25fe47690ee35bb2f7bd201590af85218aee927c
404mimic <love75200156@gmail.com>
2024-06-18 15:43:12 +0900

    feat : 상점페이지 완성

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,3 +1,6 @@
+
+
+
 (function (d) {
     var config = {
         kitId: 'xfr4aba',
@@ -6,10 +9,9 @@
     },
     h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
 })(document);
-
 $(document).ready(function(){
-    $("#header").load("/header");
-    $("#nav").load("/nav");
-    $("#chat").load("/chat");
-    $("#footer").load("/footer");
-});
+        $("#header").load("./include/header.html");
+        $("#nav").load("./include/nav.html");
+        $("#chat").load("./include/chat.html");
+        $("#footer").load("./include/footer.html");
+    });
