34d09e3540a3b18d8aa5771b7bbbf22cc802ac5b
Mr. Hong <rammus1018@naver.com>
2024-07-01 16:44:48 +0900

    feat : 로그아웃 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,6 +1,10 @@
-
 $(document).ready(function(){
     $("#header").load("/header");
     $("#nav").load("/nav");
     $("#chat").load("/chat");
 });
+
+function logout() {
+    window.location.href = '/logout';
+    alert('정상적으로 로그아웃 하였습니다.');
+}
