070c42f635f939a2d64d1db2906bb9a3fad1479f
Mr. Hong <rammus1018@naver.com>
2024-07-01 17:27:10 +0900

    feat : 마이페이지 추가

--- src/main/java/com/glossy/evolchat/controller/AjaxPageController.java
+++ src/main/java/com/glossy/evolchat/controller/AjaxPageController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AjaxPageController {
+
     //  에볼루션
     @GetMapping("/home_page")
     public String home_page() {
@@ -13,14 +14,12 @@ public class AjaxPageController {
     }
 
     //  오픈채팅
-
     @GetMapping("/openchat_page")
     public String openchat_page() {
         return "openchat";
     }
 
     //  커뮤니티
-
     @GetMapping("/community_page")
     public String community_page() {
         return "community";
@@ -42,21 +41,18 @@ public class AjaxPageController {
     }
 
     //  랭킹
-
     @GetMapping("/rank_page")
     public String rank_page() {
         return "rank";
     }
 
     //  상점
-
     @GetMapping("/shopping_page")
     public String shopping_page() {
         return "shopping";
     }
 
     //  고객센터
-
     @GetMapping("/support_help_page")
     public String support_help_page() {
         return "support_help";
@@ -108,6 +104,35 @@ public class AjaxPageController {
     @GetMapping("/messenger_message_page")
     public String messenger_message_page() {
         return "messenger_message";
+    }
+
+    // 마이페이지
+
+    @GetMapping("/mypqge_page")
+    public String my_page_page() {
+        return "mypqge";
+    }
+
+    @GetMapping("/my_myitem_page")
+    public String my_myitem_page() {
+        return "my_myitem";
+    }
+
+    @GetMapping("/my_mycash_page")
+    public String my_mycash_page() {
+        return "my_mycash";
+    }
+    @GetMapping("/my_activitypoints_page")
+    public String my_activitypoints_page() {
+        return "my_activitypoints";
+    }
+    @GetMapping("/my_goldchip_page")
+    public String my_goldchip_page() {
+        return "my_goldchip";
+    }
+    @GetMapping("/my_mypqge_page")
+    public String my_mypqge_page() {
+        return "my_mypqge";
     }
 
 }
