1364e0c97997d2fa5fe08eb8600c52be01c0aa94
shinjaehoon98 <kiki9050@gmail.com>
2024-09-10 17:54:00 +0900

    fix : 최근메시지 드롭박스 채팅추가, 오픈채팅 플레이스 수정

--- src/main/resources/static/js/openchat.js
+++ src/main/resources/static/js/openchat.js
@@ -16,35 +16,37 @@ $(document).ready(function() {
                     const roomElement = document.createElement('div');
                     roomElement.className = 'room';
 
-                    roomElement.innerHTML = `
-                        <div class="photo-layout">
-                            <div class="img">
-                                <img src="../../static/images/photo/photo_1.jpg" alt="#">
-                            </div>
-                        </div>
-                        <div class="text-wrap position-r">
-                            <div class="position-a">
-                                <img class="profile-img-52" src="../../static/images/profile/default.png" alt="#">
-                            </div>
-                            <div class="v-h"></div>
-                            <div class="top">
-                                <div class="tit-wrap white px17 m-b-10">
-                                    ${room.name}
-                                </div>
-                                <div class="summary px14">
-                                    ${room.description}
+                     roomElement.innerHTML = `
+                        <a href="openchat_place" class="room-link">
+                            <div class="photo-layout">
+                                <div class="img">
+                                    <img src="../../static/images/photo/photo_1.jpg" alt="#">
                                 </div>
                             </div>
-                            <div class="v-h"></div>
-                            <div class="bottom flex-c">
-                                <img src="../../static/images/svg/people.svg" alt="" style="height: auto; width: auto;" class="m-r-4"/>
-                                <span class="opacity60 px14 m-r-14">${room.participants}</span>
-                                <img src="../../static/images/svg/hearts.svg" alt="" style="height: auto; width: auto;" class="m-r-4"/>
-                                <span class="opacity60 px14 m-r-14">${room.likes}</span>
-                                <img src="../../static/images/svg/star.svg" alt="" style="height: auto; width: auto;" class="m-r-4"/>
-                                <span class="opacity60 px14 m-r-14">${room.stars}</span>
+                            <div class="text-wrap position-r">
+                                <div class="position-a">
+                                    <img class="profile-img-52" src="../../static/images/profile/default.png" alt="#">
+                                </div>
+                                <div class="v-h"></div>
+                                <div class="top">
+                                    <div class="tit-wrap white px17 m-b-10">
+                                        ${room.name}
+                                    </div>
+                                    <div class="summary px14">
+                                        ${room.description}
+                                    </div>
+                                </div>
+                                <div class="v-h"></div>
+                                <div class="bottom flex-c">
+                                    <img src="../../static/images/svg/people.svg" alt="" style="height: auto; width: auto;" class="m-r-4"/>
+                                    <span class="opacity60 px14 m-r-14">${room.participants}</span>
+                                    <img src="../../static/images/svg/hearts.svg" alt="" style="height: auto; width: auto;" class="m-r-4"/>
+                                    <span class="opacity60 px14 m-r-14">${room.likes}</span>
+                                    <img src="../../static/images/svg/star.svg" alt="" style="height: auto; width: auto;" class="m-r-4"/>
+                                    <span class="opacity60 px14 m-r-14">${room.stars}</span>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     `;
 
                     roomLayer.appendChild(roomElement);
